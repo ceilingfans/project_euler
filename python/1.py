3a6f79e8a6c018a8d03060ac0d88a259b155bbba
awned <65061212+ceilingfans@users.noreply.github.com>
2022-03-23 07:14:16 +0800

    refactor(1): use lambda function

--- python/1.py
+++ python/1.py
@@ -1,14 +1,7 @@
 from functools import reduce
 
-
-def reducer(a, b):
-    if not b % 3 or not b % 5:
-        return a + b
-    return a
-
-
 def solve():
-    return reduce(reducer, range(1000))
+    return reduce(lambda a, b: a + b if not b % 3 or not b % 5 else a, range(1000))
 
 
 if __name__ == "__main__":
